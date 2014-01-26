907293c42784e913c5ff09372b50e3e4e859e4d8
Nathan Abraham <nabraham@somedomain.com>
2014-01-26 13:41:54 -0500

    refactor + 2 more charts

--- public/js/spreadsheet.js
+++ public/js/spreadsheet.js
@@ -4,61 +4,65 @@ var url =
     "/od6/public/values?alt=json-in-script&callback=?";
 
 var titleUrl = 'https://docs.google.com/spreadsheet/pub?key=0AgBEpBEeCWyVcnIyd2RWV3dxRThQYXZCdWhNWXBEZmc&single=true&gid=0&output=html'
-
-var field_names = [
-    'calculated',
-    'date',
-    'display',
-    'g',
-    'gallons',
-    'odo',
-    'station',
-    'total',
-    'trip',
+var identity = function(x) { return x };
+var fields = [
+    {name: 'calculated', parser: parseFloat},
+    {name: 'date', parser: Date.parse},
+    {name: 'display', parser: parseFloat},
+    {name: 'g', parser: parseFloat},
+    {name: 'gallons', parser: parseFloat},
+    {name: 'odo', parser: parseInt},
+    {name: 'station', parser: identity},
+    {name: 'total', parser: parseFloat},
+    {name: 'trip', parser: parseInt}
 ]; 
 
+var fieldValues = {};
+fields.forEach(function(item) {
+    fieldValues[item.name] = [];
+});
+
+var fieldsNonZero = function(fieldNames, entry) {
+    for (f in fieldNames) {
+        if (entry['gsx$' + fieldNames[f]].$t.length < 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var plotxy = function(xsr, ys, names, title, subtitle, ylabel, elementId) {
+    Plot.xy({
+        xs: xsr,
+        xtype: 'datetime',
+        ys: ys,
+        names: names,
+        title: title,
+        subtitle: '',
+        ylabel: ylabel,
+        charttype: 'spline',
+        elementId: elementId
+    });
+}
 
 $.getJSON(url,{}, function (data) { 
-    dates = [];
-    calc = [];
-    display = [];
-    odo = [];
     for (var i=3; i<data.feed.entry.length; i++) {
         var entry = data.feed.entry[i];
-        var dateString = entry.gsx$date.$t;
-        var calcString = entry.gsx$calculated.$t;
-        var displayString = entry.gsx$display.$t;
-        var odoString = entry.gsx$odo.$t;
-        if (dateString.length > 0 && calcString.length > 0) {
-            dates.push( Date.parse(dateString) );
-            calc.push(  parseFloat(calcString) );
-            display.push( parseFloat(displayString) );
-            odo.push( parseInt(odoString) );
+        if (fieldsNonZero(['date', 'calculated', 'display'], entry)) {
+            fields.forEach(function(item) {
+                fieldValues[item.name].push(item.parser(entry['gsx$' + item.name].$t));
+            });
         } 
     }
-    scalc = Convolve.conv(calc, Convolve.flat(5));
-    sdisp = Convolve.conv(display, Convolve.flat(5));
-    Plot.xy({
-        xs: [dates], 
-        xtype: 'datetime', 
-        ys: [calc, scalc, display, sdisp],
-        names: ['Calculated', 'Calculated-Smooth', 'Display', 'Display-Smooth'],
-        title: 'Mileage v Time', 
-        subtitle: '<a href="' + titleUrl + '">google doc source</a>', 
-        ylabel: 'Mileage (mpg)', 
-        charttype: 'spline', 
-        elementId: '#current-mileage'
-    });
-
-    Plot.xy({
-        xs: [dates], 
-        xtype: 'datetime', 
-        ys: [odo],
-        names: ['odometer'],
-        title: 'Odometer v Time', 
-        subtitle: '', 
-        ylabel: 'Odometer (miles)', 
-        charttype: 'spline', 
-        elementId: '#odo'
-    });
+    fieldValues.scalc = Convolve.conv(fieldValues.calculated, Convolve.flat(5));
+    fieldValues.sdisp = Convolve.conv(fieldValues.display, Convolve.flat(5));
+    plotxy([fieldValues.date],
+        [fieldValues.calculated, fieldValues.scalc, fieldValues.display, fieldValues.sdisp],
+        ['Calculated', 'Calculated-Smooth', 'Display', 'Display-Smooth'],
+        'Mileage v Time', '<a href="' + titleUrl + '">google doc source</a>', 
+        'Mileage (mpg)', '#current-mileage');
+    plotxy([fieldValues.date], [fieldValues.odo], ['odometer'], 'Odometer v Time', '', 
+        'Odometer (miles)', '#odo');
+    plotxy( [fieldValues.date], [fieldValues.g], ['price'], 'Price v Time', '', 'Price ($/gallon)', 
+         '#price');
 });
